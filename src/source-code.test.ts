2817810cc4dfefad4bc18986665634828249d418
Nick Nekilov <nick@scaleton.io>
2024-12-26 21:51:04 +0400

    reformat `source-code.test.ts`

--- src/source-code.test.ts
+++ src/source-code.test.ts
@@ -2,37 +2,43 @@ import { extractGetMethods } from './source-code';
 
 describe('extractGetMethods', () => {
   it('extracts get-methods names', () => {
-    expect(extractGetMethods(`
+    expect(
+      extractGetMethods(`
       get test_case_1() {}
-    `)).resolves.toEqual([
+    `),
+    ).resolves.toEqual([
       {
         methodName: 'test_case_1',
-      }
+      },
     ]);
   });
 
   it('extracts get-methods @method_id', () => {
-    expect(extractGetMethods(`
+    expect(
+      extractGetMethods(`
       @method_id(5858)
       get test_case_1() {}
-    `)).resolves.toEqual([
+    `),
+    ).resolves.toEqual([
       {
         methodName: 'test_case_1',
         methodId: 5858,
-      }
+      },
     ]);
   });
 
   it('extracts get-methods doc-blocks', () => {
-    expect(extractGetMethods(`
+    expect(
+      extractGetMethods(`
       // @exitCode 50
       // @gasLimit
       get test_case_1() {}
-    `)).resolves.toEqual([
+    `),
+    ).resolves.toEqual([
       {
         methodName: 'test_case_1',
         docBlock: '@exitCode 50\n@gasLimit',
-      }
+      },
     ]);
   });
 });
